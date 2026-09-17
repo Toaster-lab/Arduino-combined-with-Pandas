6c77fee0516a3cfe9fd5cee12467c1cd4a8955cf
Thorben Koch <cook.thorben@gmail.com>
2026-09-17 20:03:21 +0200

    Planning of my second circuit

--- Programms/save_data_to_file.py
+++ Programms/save_data_to_file.py
@@ -12,3 +12,5 @@ with open("temperature.txt", "w") as file:
        file.write(data + "\n")
        #print(data) <= to check if it even recieves data
        file.flush()
+#I am going to need to create a list with the data I get from Serial
+#My data should be in a specific order so I can assign each column their respecting name.
