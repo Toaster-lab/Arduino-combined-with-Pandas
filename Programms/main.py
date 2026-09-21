70a47939b6e74dfafc749fc14babaadc3128f6b6
Thorben Koch <cook.thorben@gmail.com>
2026-09-21 23:45:20 +0200

    photo of circuit

--- Programms/main.py
+++ Programms/main.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib
 
 
-data = pd.read_csv("example_file.txt", sep = " ", names = ["tempature", "humidity", "light", "rain"], index_col="Time")
+data = pd.read_csv("example_file.txt", sep = " ", names = ["tempature", "humidity", "light", "rain", "time"], index_col="time")
 #this is an example, the order of information is not fixed yet
 #this is gonna give me data set to work with
+
+
 
