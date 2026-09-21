4965715c1bebc8ab348865ee8f02cf4c63d45a5d
Thorben Koch <cook.thorben@gmail.com>
2026-09-21 19:37:41 +0200

    Test

--- Programms/main.py
+++ Programms/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 
 
-data = pd.read_csv("example_file.txt", sep = " ", names = ["tempature", "humidity", "light", "rain"])
+data = pd.read_csv("example_file.txt", sep = " ", names = ["tempature", "humidity", "light", "rain"], index_col="Time")
 #this is an example, the order of information is not fixed yet
 #this is gonna give me data set to work with
+
