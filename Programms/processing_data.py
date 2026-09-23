e8996133b8c07503e905b29fc8bf1dc463ed87e6
Thorben Koch <cook.thorben@gmail.com>
2026-09-23 20:21:22 +0200

    resolving issue

---
+++ Programms/processing_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("example_file.txt",
+                    sep = " ",
+                    names = ["time","tempature", "humidity", "light", "rain"], 
+                    index_col="time"
+                    )
+
+xpoints = data.index
+ypoints = data["tempature"]
+
+plt.plot(xpoints, ypoints)
+plt.show()
+plt.xlabel("Timestamp")
+plt.ylabel("Tempature in Celcius")
+#I sadly dont have that much time to programm because of school 
+#this is an example, the order of information is not fixed yet
+#this is gonna give me data set to work with
