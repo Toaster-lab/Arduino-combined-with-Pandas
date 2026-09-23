acc1dde5fc2faf9f2a91bb59f39f9367566ac569
Thorben Koch <cook.thorben@gmail.com>
2026-09-23 20:09:52 +0200

    trying to resolve issue

--- Programms/main.py
+++ Programms/main.py
@@ -2,9 +2,7 @@ import pandas as pd
 import numpy as np
 <<<<<<< HEAD
 import matplotlib.pyplot as plt
-=======
-import matplotlib
->>>>>>> e96fefe25ae6e8f5a05e237cb09b4ff8fe2f313b
+
 
 data = pd.read_csv("example_file.txt",
                     sep = " ",
@@ -12,7 +10,6 @@ data = pd.read_csv("example_file.txt",
                     index_col="time"
                     )
 
-<<<<<<< HEAD
 xpoints = data.index
 ypoints = data["tempature"]
 
@@ -20,11 +17,5 @@ plt.plot(xpoints, ypoints)
 plt.show()
 
 #I sadly dont have that much time to programm because of school 
-=======
-data = pd.read_csv("example_file.txt", sep = " ", names = ["tempature", "humidity", "light", "rain", "time"], index_col="time")
->>>>>>> e96fefe25ae6e8f5a05e237cb09b4ff8fe2f313b
 #this is an example, the order of information is not fixed yet
 #this is gonna give me data set to work with
-
-
-
