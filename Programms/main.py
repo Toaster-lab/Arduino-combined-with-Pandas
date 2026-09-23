9058b410f2a04d6b791754b239b13acf6f58a1ce
Thorben Koch <cook.thorben@gmail.com>
2026-09-23 20:00:55 +0200

    Making tempature diagram

--- Programms/main.py
+++ Programms/main.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
+data = pd.read_csv("example_file.txt",
+                    sep = " ",
+                    names = ["time","tempature", "humidity", "light", "rain"], 
+                    index_col="time"
+                    )
 
-data = pd.read_csv("example_file.txt", sep = " ", names = ["tempature", "humidity", "light", "rain"])
+xpoints = data.index
+ypoints = data["tempature"]
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+#I sadly dont have that much time to programm because of school 
 #this is an example, the order of information is not fixed yet
 #this is gonna give me data set to work with
